121379142504e86188beff00e21012cdf77df475
Veni <44931124+Venicode@users.noreply.github.com>
2023-10-16 11:40:21 -0300

    Update codigoAjustado.cpp

--- codigoAjustado.cpp
+++ codigoAjustado.cpp
@@ -1,14 +1,23 @@
+int duracao = 0;
+int distancia = 0;
+
+float trigger = 12;
+float echo = 11;
+
 const int motorD =  5;
 const int motorE = 6;
 const int Sensor1 = A0;
 const int Sensor2 = A1;
+
 int valorSensor1 = 0;
 int valorSensor2 = 0;
 
+const int ledVerde = 13;
+
 void setup() {
- pinMode(13, OUTPUT);
+ pinMode(ledVerde, OUTPUT);
  delay(5000);
- digitalWrite(13,HIGH);
+ digitalWrite(ledVerde,HIGH);
   
  pinMode(motorD, OUTPUT);
  pinMode(motorE, OUTPUT);
@@ -16,38 +25,51 @@ void setup() {
  pinMode(Sensor1, INPUT);
  pinMode(Sensor2, INPUT);
   
+ pinMode(trigger, OUTPUT);
+ pinMode(echo, INPUT);
+  
  Serial.begin(9600);
 }
 
 void loop() {
- digitalWrite(motorD, HIGH);
- digitalWrite(motorE, HIGH);
- valorSensor1 = analogRead(Sensor1);
- valorSensor2 = analogRead(Sensor2);
- Serial.print("Leitura do Sensor1: ");
- Serial.println(valorSensor1);
- Serial.print("Leitura do Sensor2: ");
- Serial.println(valorSensor2);
   
-   if ((valorSensor1 > 900) && (valorSensor2 > 900)){
-        digitalWrite(motorD, HIGH);
-        digitalWrite(motorE, HIGH);
-    }
+  digitalWrite(motorD, HIGH);
+  digitalWrite(motorE, HIGH);
+  valorSensor1 = analogRead(Sensor1);
+  valorSensor2 = analogRead(Sensor2);
+  
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
 
-    if ((valorSensor1 < 900) && (valorSensor2 > 900)){
-        digitalWrite(motorD, LOW);
-        digitalWrite(motorE, HIGH);
-    }
-
-    if ((valorSensor1 > 900) && (valorSensor2 < 900)){
-        digitalWrite(motorD, HIGH);
-        digitalWrite(motorE, LOW);
-    }
+  duracao = pulseIn(echo, HIGH);
+  distancia = duracao*0.017175;
+  
+if (distancia>5){
+ 
+ if ((valorSensor1 > 900) && (valorSensor2 > 900)){
+     digitalWrite(motorD, HIGH);
+     digitalWrite(motorE, HIGH);
+ }
+ 
+ else if ((valorSensor1 < 900) && (valorSensor2 > 900)){
+     digitalWrite(motorD, LOW);
+     digitalWrite(motorE, HIGH);
+ }
+ 
+ else if ((valorSensor1 > 900) && (valorSensor2 < 900)){
+     digitalWrite(motorD, HIGH);
+     digitalWrite(motorE, LOW);
+ }
   	
-  	if ((valorSensor1 < 900) && (valorSensor2 < 900)){
-        digitalWrite(motorD, LOW);
-        digitalWrite(motorE, LOW);
-    }
-  
- delay(1000);
+ else {
+     digitalWrite(motorD, LOW);
+     digitalWrite(motorE, LOW);
+ }
+} 
+else {
+    Serial.println("Objeto detectado");
+  	 digitalWrite(motorD, LOW);
+    digitalWrite(motorE, LOW);
+ }
 }
