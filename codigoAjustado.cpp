88ac374c8e6fca5382643e766d289042e49031f1
Veni <44931124+Venicode@users.noreply.github.com>
2023-10-16 12:42:43 -0300

    Update codigoAjustado.cpp

--- codigoAjustado.cpp
+++ codigoAjustado.cpp
@@ -1,23 +1,21 @@
-int duracao = 0;
-int distancia = 0;
-
-float trigger = 12;
-float echo = 11;
-
+const float trigger = 12;
+const float echo = 11;
 const int motorD =  5;
 const int motorE = 6;
 const int Sensor1 = A0;
 const int Sensor2 = A1;
+const int ledVerde = 13;
+const int ledVermelho = 10;
 
 int valorSensor1 = 0;
 int valorSensor2 = 0;
-
-const int ledVerde = 13;
+int duracao = 0;
+int distancia = 0;
 
 void setup() {
+ pinMode(ledVermelho, OUTPUT);
  pinMode(ledVerde, OUTPUT);
  delay(5000);
- digitalWrite(ledVerde,HIGH);
   
  pinMode(motorD, OUTPUT);
  pinMode(motorE, OUTPUT);
@@ -33,10 +31,13 @@ void setup() {
 
 void loop() {
   
-  digitalWrite(motorD, HIGH);
-  digitalWrite(motorE, HIGH);
   valorSensor1 = analogRead(Sensor1);
   valorSensor2 = analogRead(Sensor2);
+  
+  Serial.print("Leitura do Sensor1: ");
+  Serial.println(valorSensor1);
+  Serial.print("Leitura do Sensor2: ");
+  Serial.println(valorSensor2);
   
   digitalWrite(trigger, HIGH);
   delayMicroseconds(10);
@@ -46,7 +47,10 @@ void loop() {
   distancia = duracao*0.017175;
   
 if (distancia>5){
- 
+  
+ digitalWrite(ledVerde,HIGH);
+ digitalWrite(ledVermelho, LOW);
+  
  if ((valorSensor1 > 900) && (valorSensor2 > 900)){
      digitalWrite(motorD, HIGH);
      digitalWrite(motorE, HIGH);
@@ -68,8 +72,15 @@ if (distancia>5){
  }
 } 
 else {
-    digitalWrite(motorD, LOW);
-    digitalWrite(motorE, LOW);
-    Serial.println("Objeto detectado");
+  	Serial.println("Objeto detectado");
+  	digitalWrite(ledVerde, LOW);
+  	digitalWrite(ledVermelho, HIGH);
+   digitalWrite(motorD, LOW);
+   digitalWrite(motorE, LOW);
+  	delay(5000);
+  	digitalWrite(motorD, HIGH);
+  	digitalWrite(motorE, LOW);
+  	delay(5000);
+    
  }
 }
