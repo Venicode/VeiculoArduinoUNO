7996f661ab504882acccd39e192acedcf8f8baae
Veni <44931124+Venicode@users.noreply.github.com>
2023-10-16 13:22:50 -0300

    Update codigoAjustado.cpp

--- codigoAjustado.cpp
+++ codigoAjustado.cpp
@@ -1,3 +1,4 @@
+//Declaração de variáveis de acordo com as portas que estão conectadas
 const float trigger = 12;
 const float echo = 11;
 const int motorD =  5;
@@ -7,11 +8,16 @@ const int Sensor2 = A1;
 const int ledVerde = 13;
 const int ledVermelho = 10;
 
+/*Declarando as variaveis que irão armazenar valores 
+de acordo com a leitura dos sensores */
 int valorSensor1 = 0;
 int valorSensor2 = 0;
 int duracao = 0;
 int distancia = 0;
 
+//Configurações iniciais
+//O input significa que o valor será obtido a partir da leitura, usado para os sensores.
+//O Output significa que as variaveis irão apenas retornar um valor, sem obter nada do externo.
 void setup() {
  pinMode(ledVermelho, OUTPUT);
  pinMode(ledVerde, OUTPUT);
@@ -26,28 +32,33 @@ void setup() {
  pinMode(trigger, OUTPUT);
  pinMode(echo, INPUT);
   
- Serial.begin(9600);
+ Serial.begin(9600); //é usado para definir uma frequencia dos dados expostos no print (9600).
 }
 
 void loop() {
-  
+  //Leitura dos valores dos sensores infravermelhos
   valorSensor1 = analogRead(Sensor1);
   valorSensor2 = analogRead(Sensor2);
-  
+
+  //Exibição dos dados no prompt da IDE
   Serial.print("Leitura do Sensor1: ");
   Serial.println(valorSensor1);
   Serial.print("Leitura do Sensor2: ");
   Serial.println(valorSensor2);
-  
+
+  //O trigger emite o pulso de onda a cada 10 microssegundos
   digitalWrite(trigger, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigger, LOW);
 
+  //O echo recebe o pulso retornado e armazena da duracao
   duracao = pulseIn(echo, HIGH);
+  //Calculo para encontrar a distância, o valor fixo é definido pelo cálculo da velocidade da onda ultrassônica
   distancia = duracao*0.017175;
-  
-if (distancia>5){
-  
+
+/*Os sensores infravermelhos retornam valores dependendo da luz refletida.
+Se for branco, retorna menos de 900. Se for preto, retorna superior. */
+if (distancia>5){ 
  digitalWrite(ledVerde,HIGH);
  digitalWrite(ledVermelho, LOW);
   
@@ -70,7 +81,9 @@ if (distancia>5){
      digitalWrite(motorD, LOW);
      digitalWrite(motorE, LOW);
  }
-} 
+}
+/*O if acima verifica se a distância é superior a 5cm. 
+Caso for igual ou inferior, irá executar o comando do else*/
 else {
   	Serial.println("Objeto detectado");
   	digitalWrite(ledVerde, LOW);
@@ -81,6 +94,5 @@ else {
   	digitalWrite(motorD, HIGH);
   	digitalWrite(motorE, LOW);
   	delay(2000);
-    
  }
 }
